68a1176be74962d258ae7c65ecf8089e9ea1881f
matthcarson01 <matthcarson01@gmail.com>
2021-08-27 15:00:55 -0400

    styling and positioning buttons and display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h2>Calculator</h2>
       </header>
-    </div>
+      <section id="display"></section>
+      <section id="keys">
+        <div id="nine" className="btn bkg-ltgry">9</div>
+        <div id="eight" className="btn bkg-ltgry">8</div>
+        <div id="seven" className="btn bkg-ltgry">7</div>
+        <div id="six" className="btn bkg-ltgry">6</div>
+        <div id="five" className="btn bkg-ltgry">5</div>
+        <div id="four" className="btn bkg-ltgry">4</div>
+        <div id="three" className="btn bkg-ltgry">3</div>
+        <div id="two" className="btn bkg-ltgry">2</div>
+        <div id="one" className="btn bkg-ltgry">1</div>
+        <div id="zero" className="btn-2 bkg-ltgry">0</div>
+        <div id="decimal" className="btn bkg-ltgry">.</div>
+        <div id="divide" className="btn bkg-gry">/</div>
+        <div id="multiply" className="btn bkg-gry">*</div>
+        <div id="subtract" className="btn bkg-gry">-</div>
+        <div id="add" className="btn bkg-gry">+</div>
+        <div id="equals" className="btn-3 bkg-gry">=</div>
+        <div id="clear" className="btn bkg-rd">AC</div>
+      </section>
+    </main>
   );
 }
 
